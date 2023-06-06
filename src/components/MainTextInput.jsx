e49b5e012a70164f83c9dc37e8d994020f179be5
dev-deeplake <108051058+Jvit-togle@users.noreply.github.com>
2023-06-06 20:58:44 +0900

    Main : enter / shift+enter 키 매핑 커리

--- src/components/MainTextInput.jsx
+++ src/components/MainTextInput.jsx
@@ -33,9 +33,27 @@ function MainTextInput({ handleSubmit, focusedChat }) {
     };
     countLines();
   }, [inputValue]);
+
+  const submitHandler = async () => {
+    // mutation과 연결되어있기 때문에 비동기처리를 실행하며, 그렇기 때문에 input을 비워주기 위해서는 해당 비동기 처리가 끝나고 나서 setInputValue를 초기화해야 함
+    if (focusedChat === null) { // 초점 맞춰진 대화가 없으면 새 대화 시작, 그렇지 않을시 대화 이어나가기
+      await handleSubmit(inputValue);
+    } else {
+      await handleSubmit(inputValue, focusedChat)
+    }
+    setInputValue(INIT_INPUT_VALUE) // InputValue 비워주기
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter" && !event.shiftKey && !event.nativeEvent.isComposing) { // event.nativeEvent.isComposing이 false일 때만 동작 (맨 마지막 글자 재출력 방지)
+      event.preventDefault() // 엔터키 쳤을 때의 줄바꿈 막기
+      submitHandler()
+    }
+  }
+
   return (
     <style.MainInputContainer>
-      <style.MainInput onChange={(event) => setInputValue(event.target.value)} value={inputValue} height={inputHeight} placeholder="Send a message..."></style.MainInput>
+      <style.MainInput onKeyDown={handleKeyDown} onChange={(event) => setInputValue(event.target.value)} value={inputValue} height={inputHeight} placeholder="Send a message..."></style.MainInput>
       <Send iconColor={sessionStorage.getItem("userHex")} isContent={!!inputValue} handleSubmit={focusedChat === null ? () => handleSubmit(inputValue) : () => handleSubmit(inputValue, focusedChat)} />
     </style.MainInputContainer>
   );
